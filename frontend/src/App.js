e8a36ee95fc441d67d065d3995b6fc975b6bbc5a
Vicente Rocha <vicente@Vicentes-MacBook-Pro.local>
2022-11-13 17:19:01 +0000

    Add form to enter lottery

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import web3 from './web3';
- 
+import {ethers} from 'ethers';
+import Lottery from './contracts/Lottery.json';
+import contractAddress from './contracts/contract-address.json';
+
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+       manager: '',
+       players: [],
+       lotteryBalance: ethers.BigNumber.from("0"),
+       value: ''
+    };
+  }
+
+  async componentDidMount() {
+    // Get Provider
+    this._provider = new ethers.providers.Web3Provider(window.ethereum);
+    // Get the contract using the Provider
+    this._lottery = new ethers.Contract(contractAddress.Lottery, Lottery.abi, this._provider);
+
+    // Get the manager from the contract
+    const manager = await this._lottery.manager();
+    const players = await this._lottery.getPlayers();
+    const lotteryBalance = await this._provider.getBalance(this._lottery.address);
+
+    this.setState({ manager, players, lotteryBalance });
+  
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await this._provider.listAccounts();
+    await this._lottery.enter( {from: accounts[0], value: ethers.utils.parseEther(this.state.value)} )
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>This contract is managed by {this.state.manager}</p>
+        <br/>
+        <p>Current lottery value is {ethers.utils.formatEther(this.state.lotteryBalance)}!</p>
+        <p>This are the currently {this.state.players.length} players in the lottery.</p>
+      <hr/>
+
+      <form onSubmit={this.onSubmit}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input
+          value={this.state.value}
+            onChange={event => this.setState({ value: event.target.value })}
+          ></input>
+        </div>
+        <button>Enter!</button>
+      </form>
+      
       </div>
     );
   }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,55 @@
 import { ethers } from "hardhat";
 
-async function main() {
-  const [deployer] = await ethers.getSigners();
+const path = require("path");
 
-  console.log("Deploying contracts with the account:", deployer.address);
+async function main() {
+  // This is just a convenience check
+  if (network.name === "hardhat") {
+    console.warn(
+      "You are trying to deploy a contract to the Hardhat Network, which" +
+        "gets automatically created and destroyed every time. Use the Hardhat" +
+        " option '--network localhost'"
+    );
+  }
+
+  // ethers is available in the global scope
+  const [deployer] = await ethers.getSigners();
+  console.log(
+    "Deploying the contracts with the account:",
+    await deployer.getAddress()
+  );
 
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  const Inbox = await ethers.getContractFactory("Inbox");
-  const inbox = await Inbox.deploy("My first test network message");
+  const Lottery = await ethers.getContractFactory("Lottery");
+  const lottery = await Lottery.deploy();
+  await lottery.deployed();
 
-  console.log("Inbox address:", inbox.address);
+  console.log("Lottery address:", lottery.address);
+
+  // We also save the contract's artifacts and address in the frontend directory
+  saveFrontendFiles(lottery);
+}
+
+function saveFrontendFiles(lottery) {
+  const fs = require("fs");
+  const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    path.join(contractsDir, "contract-address.json"),
+    JSON.stringify({ Lottery: lottery.address }, undefined, 2)
+  );
+
+  const LotteryArtifact = artifacts.readArtifactSync("Lottery");
+
+  fs.writeFileSync(
+    path.join(contractsDir, "Lottery.json"),
+    JSON.stringify(LotteryArtifact, null, 2)
+  );
 }
 
 main()
