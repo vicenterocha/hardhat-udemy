3bd7b7572982e255539af75473b2fd52905a8efb
Vicente Rocha <vicente@Vicentes-MacBook-Pro.local>
2022-11-19 12:20:05 +0000

    Add campaign contract

---
+++ contracts/Campaign.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+    
+    function createCampaign(uint minimum) public {
+        Campaign newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(address(newCampaign));
+    }
+    
+    function getDeployedCampaigns() public view returns (address[] memory) {  // todo: check why it cant be calldata
+        return deployedCampaigns;
+    }
+}
+
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint amount;
+        address recipient;
+        bool complete;
+        uint approvalsCount;
+        mapping(address => bool) approvals;
+    }
+
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+
+    modifier restricted() {
+        require(msg.sender == manager, "Call restricted to the manager");
+        _;
+    }
+
+    constructor (uint minimum, address creator) {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution, "Minimum required contribution not met");
+
+        approvers[msg.sender] = true;
+        approversCount ++;
+    }
+
+    function createRequest(string calldata description, uint amount, address recipient) public restricted {
+        Request storage newRequest = requests.push();
+        newRequest.description = description;
+        newRequest.amount = amount;
+        newRequest.recipient = recipient;
+        newRequest.complete = false;
+        newRequest.approvalsCount = 0;
+    }
+
+    function approve(uint requestIndex) public {
+        Request storage request = requests[requestIndex];  // it's storage because we want changes
+        // to this variable to be reflected in our requests storage variable.
+
+        require(approvers[msg.sender], "Only donators can approve");
+        require(!request.approvals[msg.sender], "Can only approve once");
+
+        request.approvals[msg.sender] = true;
+        request.approvalsCount ++;
+    }
+
+    function finalizeRequest(uint requestIndex) public restricted {
+        Request storage request = requests[requestIndex];
+        
+        require(request.approvalsCount > (approversCount / 2));
+        require(!request.complete, "Request can only be finalized once");
+
+        payable(request.recipient).transfer(request.amount);
+        request.complete = true;
+    }
+}
